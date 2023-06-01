a060dac6bc733981d2e86d702c87f73e14e11ae3
kriakemor <vjoelsuxod@gmail.com>
2023-06-01 18:27:17 -0400

    vite nuevo

--- vite.config.js
+++ vite.config.js
@@ -3,11 +3,7 @@ import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
 
 export default defineConfig({
-    server: {
-        host: 'localhost',
-        port: 7563,
-        cors: true
-      },
+
 
     plugins: [
         laravel({
