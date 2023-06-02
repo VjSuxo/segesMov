52b0242b542b60514e04cfaba710028623bc5c48
kriakemor <vjoelsuxod@gmail.com>
2023-06-02 10:02:57 -0400

    evento index

--- resources/views/components/layouts/navigation.blade.php
+++ resources/views/components/layouts/navigation.blade.php
@@ -2,7 +2,7 @@
     <div class="container-fluid">
       <a class="navbar-brand" href="{{ route('inicio') }}">SEGES</a>
       <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-        <span class="navbar-toggler-icon border border-warning"></span>
+        <span class="navbar-toggler-icon border border-warning">ACCIONES</span>
       </button>
       <div class="collapse navbar-collapse" id="navbarSupportedContent">
         <ul class="navbar-nav me-auto mb-2 mb-lg-0">
